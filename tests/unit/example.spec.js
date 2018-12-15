efa8d932e0f1656e484a7ded5a74e4bc0e3a246c
Nataliya Shipelova <natulechka_m.25@mail.ru>
2018-12-15 21:31:15 +0300

    feat (3D player)
    add simple test and Travis CI

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Vue from "vue";
+import App from "../../src/App.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  new Vue({
+    render: h => h(App)
+  }).$mount(div);
 });
+
